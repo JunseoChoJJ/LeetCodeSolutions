aed884390cb5bf464b7ec30043d5da9a78630ff5
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-03-05 15:03:41 -0500

    Create AssignCookies.py

---
+++ greedy/easy/AssignCookies.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        
+        
+        child = 0
+        
+        for cookie in s:    
+            
+            if cookie >= g[child]:
+                
+            
+                child += 1
+                
+            if child == len(g):
+                
+                
+                return child
+        
+        return child
