ed226f2cff40f62b9f628478731599344a6af50f
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-11-05 18:45:19 -0500

    Update 162.py

--- TopInterviewQ/medium/162.py
+++ TopInterviewQ/medium/162.py
@@ -1,5 +1,19 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        i=0
+        j=len(nums)-1
+        while(i<j):
+            mid=(i+j)//2
+            if(nums[mid]<nums[mid+1]):
+                i=mid+1
+            else:
+                j=mid
+        return i
+
+
+        '''
         newList = sorted(nums)
 
         return nums.index(newList[-1])
+
+        '''
